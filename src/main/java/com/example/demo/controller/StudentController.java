cbbcf552a5481c1060bc4b71258d5984927042db
bala-amypo <bala@amypo.in>
2025-12-17 08:57:13 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,13 @@
 package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.blind.annotation.*;
+import com.example.demo.entity.Student;
 @RestController
 
 public class StudentControlle{
-    
+    @postMapping("/addStudent")
+    public Student addStudent(@RequestBody Student st){
+        return
+    }
 }
